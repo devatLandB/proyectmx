e2e394a358d428295d040f2ae4af930202daa625
devatLandB <dev@landandbuilding.com>
2024-07-11 18:09:48 -0700

    equipo and const bgs added

--- src/scripts/hmbrgr.js
+++ src/scripts/hmbrgr.js
@@ -1,25 +1,31 @@
 
-document.addEventListener('DOMContentLoaded', function () {
+
+  document.addEventListener('DOMContentLoaded', function () {
     console.log("Hamburger menu script loaded");
-    
+
     const hamburgerBtn = document.getElementById('hamburger-btn');
     const overlay = document.getElementById('overlay');
     const closeBtn = document.getElementById('close-btn');
     const overlayLinks = document.querySelectorAll('.overlay-link');
-  
+
     const openOverlay = () => {
       overlay.classList.remove('translate-x-full');
       overlay.classList.add('translate-x-0');
     };
-  
+
     const closeOverlay = () => {
       overlay.classList.remove('translate-x-0');
       overlay.classList.add('translate-x-full');
     };
-  
+
     hamburgerBtn?.addEventListener('click', openOverlay);
     closeBtn?.addEventListener('click', closeOverlay);
     overlayLinks.forEach(link => {
       link.addEventListener('click', closeOverlay);
+      
+      // Check if the link href matches the current page path
+      if (link.href === window.location.href) {
+        link.classList.add('text-blue-500'); 
+      }
     });
   });
